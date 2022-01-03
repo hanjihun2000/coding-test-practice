1d8a22c401fdd614f26b45b4ed8630bc0cf54eac
jhhan0 <1155128719@link.cuhk.edu.hk>
2022-01-03 20:41:05 +0900

    #2 Add Two Numbers solution using python

---
+++ AddTwoNumbers/AddTwoNumbers.py
@@ -0,0 +1,30 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def addTwoNumbers(self, l1, l2):
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        """
+        result = ListNode()
+        curr = result
+        carry = 0
+        while l1 or l2 or carry:
+            val = 0
+            if l1:
+                val += l1.val
+                l1 = l1.next
+            if l2:
+                val += l2.val
+                l2 = l2.next
+            val += carry
+            carry = val // 10  # float division. ex) 16 // 7 => 2
+            val %= 10
+            node = ListNode(val=val)
+            curr.next = node
+            curr = curr.next
+        return result.next
