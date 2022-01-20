00be6c0e1c783e3bdb8867b629072a2212bdcc7a
jhhan0 <1155128719@link.cuhk.edu.hk>
2022-01-20 23:08:17 +0900

    #1512 Number of Good Pairs solution using Python

---
+++ NumberOfGoodPairs/NumberOfGoodPairs.py
@@ -0,0 +1,11 @@
+class Solution:
+    def numIdenticalPairs(self, nums: List[int]) -> int:
+        pairs = 0
+
+        for i in nums:
+            c = nums.count(i)
+            n = c * (c - 1) // 2
+            pairs += n
+            nums = [j for j in nums if j != i]
+
+        return pairs
